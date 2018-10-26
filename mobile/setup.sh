55f53219830b870aa4a0c98acaeee6009335820f
Gabetn <gabriel.negash@mail.mcgill.ca>
2018-10-26 14:32:23 -0400

    Evosuite test Branch generation

---
+++ mobile/setup.sh
@@ -0,0 +1,1 @@
+java -jar $EVOSUITE -class org.scoutant.blokish.model.AI -projectCP build/intermediates/classes/release -Duse_separate_classloader=true -criterion BRANCH -Dprint_goals=true -Dprint_missed_goals
