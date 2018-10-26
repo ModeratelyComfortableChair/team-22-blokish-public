d74747617dfab5eb904bc9e410ba336f19620b1f
Gabetn <gabriel.negash@mail.mcgill.ca>
2018-10-26 15:25:44 -0400

    generated evosuite test suite for model

---
+++ mobile/setup.sh
@@ -0,0 +1,1 @@
+java -jar $EVOSUITE -prefix org.scoutant.blokish.model -projectCP build/intermediates/classes/release -Duse_separate_classloader=true -criterion CBRANCH -Dprint_goals=true -Dprint_missed_goals
