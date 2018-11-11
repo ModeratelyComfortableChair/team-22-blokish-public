5b8422446f2e91c5ef0b1a23fe92b0e6ff0fb5d7
Mahmoud Barake <mahmoudbarake95@gmail.com>
2018-11-10 21:53:36 -0500

    updates UIIntegrationTest (still not working)

--- mobile/src/test/java/org/scoutant/blokish/view/UIIntegrationTest.java
+++ mobile/src/test/java/org/scoutant/blokish/view/UIIntegrationTest.java
@@ -1,16 +1,60 @@
 package org.scoutant.blokish.view;
 
+import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.view.Display;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.scoutant.blokish.UI;
-import static org.junit.Assert.assertNotNull;
 
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(ContextCompat.class)
 public class UIIntegrationTest {
+
+//    @Mock
+//    private ContextCompat contextCompat;
+
+//    @Mock
+//    private Context context;
+
+    @Mock
+    private Context context;
+
+    @Mock
+    private Context mockApplicationContext;
+
+    @Mock
+    private Display display;
+
+    @Mock
+    private WindowManager windowManager;
 
     @Test
     public void testCreateNewGame(){
+//        contextCompat = mock(ContextCompat.class);
+//        context = mock(Context.class);
+
+        when(context.getApplicationContext()).thenReturn(mockApplicationContext);
+        when(context.getSystemService(Context.WINDOW_SERVICE)).thenReturn(windowManager);
+        when(windowManager.getDefaultDisplay()).thenReturn(display);
+
+        PowerMockito.mockStatic(ContextCompat.class);
+//        PowerMockito.when(ContextCompat.getDrawable(any(Context.class), anyInt())).thenReturn(drawable);
+        PowerMockito.when(ContextCompat.getColor(any(Context.class), anyInt())).thenReturn(2);
+
         UI ui = new UI();
         MenuItem menuItem = Mockito.mock(MenuItem.class);
         Mockito.when(menuItem.getItemId()).thenReturn(ui.getMenuItemReplay());
