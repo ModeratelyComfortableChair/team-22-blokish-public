cc894b064a59e5e23bde8a00d1d3fdfa4217b7e3
cwdesroches <charles-william.desroches@mail.mcgill.ca>
2018-11-11 21:30:05 -0500

    Updated GameViewIntegrationTest

--- mobile/src/test/java/org/scoutant/blokish/model/GameViewIntegrationTest.java
+++ mobile/src/test/java/org/scoutant/blokish/model/GameViewIntegrationTest.java
@@ -1,62 +1,18 @@
 package org.scoutant.blokish.model;
 
-import android.content.Context;
-import android.graphics.Point;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.support.v4.content.ContextCompat;
-import android.util.DisplayMetrics;
-import android.view.Display;
-import android.view.LayoutInflater;
-import android.view.WindowManager;
-
-import android.widget.ImageButton;
+import android.view.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.scoutant.blokish.ButtonsView;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
 import org.scoutant.blokish.UI;
-
-import static junit.framework.Assert.assertFalse;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.ArgumentMatchers.isA;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.powermock.api.mockito.PowerMockito.whenNew;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ContextCompat.class, ButtonsView.class, GameView.class})
+@RunWith(RobolectricTestRunner.class)
 public class GameViewIntegrationTest {
-
-    @Mock
-    private UI context;
-
-    @Mock
-    private Context mockApplicationContext;
-
-    @Mock
-    private Display display;
-
-    @Mock
-    private DisplayMetrics metrics;
-
-    @Mock
-    private WindowManager windowManager;
-
-    @Mock
-    private LayoutInflater inflater;
-
-    @Mock
-    private UI mockUI;
 
     private GameView gameview;
 
@@ -67,36 +23,13 @@ public class GameViewIntegrationTest {
 
     @Test
     public void testPieceUIOnTouchEvent() throws Exception {
-        Display display = mock(Display.class);
-        doAnswer( new Answer() {
-            public Point answer(InvocationOnMock invocation){
-                Point arg0 = invocation.getArgument(0);
-                arg0.x = 200;
-                arg0.y = 400;
-                return arg0;
-            }
-        }).when(display).getSize(any(Point.class));
+        UI context = Robolectric.setupActivity(UI.class);
 
-        when(context.getApplicationContext()).thenReturn(mockApplicationContext);
-        when(context.getSystemService(Context.WINDOW_SERVICE)).thenReturn(windowManager);
-        when(context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).thenReturn(inflater);
-        when(windowManager.getDefaultDisplay()).thenReturn(display);
-
-        PowerMockito.mockStatic(ContextCompat.class);
-        PowerMockito.when(ContextCompat.getDrawable(any(Context.class), anyInt())).thenReturn(new ColorDrawable());
-
-        ButtonsView mockButtonsView = mock(ButtonsView.class);
-        ImageButton mockImageButton = mock(ImageButton.class);
-        when(mockButtonsView.getOk()).thenReturn(mockImageButton);
-        whenNew(ButtonsView.class).withAnyArguments().thenReturn(mockButtonsView);
+        MotionEvent me = mock(MotionEvent.class);
 
         gameview = new GameView(context);
 
-        assertFalse(gameview.onTouchEvent(null));
+        assertTrue(gameview.onTouchEvent(me));
     }
 
-    @Test
-    public void testGamePlay() throws Exception {
-
-    }
 }
