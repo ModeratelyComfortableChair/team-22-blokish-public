0e50a61422f79cf57d4b3e14a2a4540237e40a59
Jerome Daniel Marfleet <jerome.marfleet@mail.mcgill.ca>
2018-11-30 17:42:19 -0500

    Extended initial tests

--- mobile/src/androidTest/java/org/scoutant/blokish/test/RobotiumTests.java
+++ mobile/src/androidTest/java/org/scoutant/blokish/test/RobotiumTests.java
@@ -1,24 +1,27 @@
 package org.scoutant.blokish.test;
 
-import junit.framework.Assert;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.robotium.solo.Solo;
 
-import org.scoutant.blokish.UI;
+import org.scoutant.blokish.*;
+import org.scoutant.blokish.model.Square;
 
 
 public class RobotiumTests extends
         ActivityInstrumentationTestCase2<UI> {
 
     private Solo solo;
+    private UI ui;
+
 
     public RobotiumTests() {
         super(UI.class);
     }
 
     public void setUp() throws Exception {
-        solo = new Solo(getInstrumentation(), getActivity());
+        super.setUp();
+
     }
 
 
@@ -27,7 +30,41 @@ public class RobotiumTests extends
         solo.finishOpenedActivities();
     }
 
-    public void testActivity() throws Exception{
+    public void testDragFeature() throws Exception{
+        solo = new Solo(getInstrumentation(), getActivity());
+
+        solo.waitForActivity("UI", 2000);
+        ui = (UI) getActivity();
+
+        PieceUI piece = ui.game.findPiece(0, "I5");
+
+        int[] start=new int[2];
+        int[] end=new int[2];
+
+        piece.getLocationOnScreen(start);
+
+        float startX=(float)start[0]+ piece.getWidth()/2;
+        float startY =(float)start[1]+ piece.getHeight()-1;
+
+
+        float endX = 0;
+        float endY = 0;
+
+        for(Square s: ui.game.game.boards.get(0).seeds()){
+            endX = s.i*ui.game.size + ui.game.size/4;
+            endY = s.j*ui.game.size + ui.game.size/4;
+
+            break;
+        }
+
+        System.out.println("Starting Location: "+startX +" " + startY);
+        solo.drag(startX, endX, startY, endY, 200);
         solo.assertCurrentActivity("UI Supposed to Launch", UI.class);
+
+        piece.getLocationOnScreen(end);
+
+        assertNotSame("X Position of piece must be different", start[0], end[0]);
+        assertNotSame("X Position of piece must be different", start[1], end[1]);
+
     }
 }
