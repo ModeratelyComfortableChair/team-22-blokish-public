5e420b7321927783335b319bd8ad91dde35b03eb
Jerome Daniel Marfleet <jerome.marfleet@mail.mcgill.ca>
2018-11-30 19:45:58 -0500

    Adding and cancelling works. Need to finish the asserts for the tests

--- mobile/src/androidTest/java/org/scoutant/blokish/test/RobotiumTests.java
+++ mobile/src/androidTest/java/org/scoutant/blokish/test/RobotiumTests.java
@@ -19,6 +19,8 @@ public class RobotiumTests extends
 
     private Solo solo;
     private UI ui;
+    private final String FIRST_PIECE_TYPE = "I5";
+    private final int COLOR = 0;
 
 
     public RobotiumTests() {
@@ -43,7 +45,7 @@ public class RobotiumTests extends
         solo.waitForActivity("UI", 2000);
         ui = (UI) getActivity();
 
-        PieceUI piece = ui.game.findPiece(0, "I5");
+        PieceUI piece = ui.game.findPiece(0, FIRST_PIECE_TYPE);
 
         int[][] result = new int[2][2];
 
@@ -76,7 +78,7 @@ public class RobotiumTests extends
         return result;
     }
 
-    private void confirmFirstPiece(){
+    private void confirmPiece(){
         int[] position = {10, 10};
         int width = 0;
         for (int i=0; i<ui.game.getChildCount(); i++) {
@@ -94,6 +96,25 @@ public class RobotiumTests extends
 
         }
     }
+    private void cancelPiece(){
+        int[] position = {10, 10};
+        int width = 0;
+        for (int i=0; i<ui.game.getChildCount(); i++) {
+            View v = ui.game.getChildAt(i);
+            if (v instanceof ButtonsView) {
+
+                ButtonsView myButtonView = (ButtonsView) v;
+                myButtonView.ok.getLocationOnScreen(position);
+                width = myButtonView.ok.getWidth();
+                position[0] =  ui.game.size*20 - (position[0] + width/2);
+                System.out.println("Found Button= "+(position[0] + width/2) +" " + (position[1] + width/2));
+                solo.clickOnScreen(position[0], position[1] + width/2);
+                break;
+            }
+
+
+        }
+    }
 
     public void testDragFeature(){
 
@@ -103,7 +124,7 @@ public class RobotiumTests extends
         } catch (Exception e){
 
         }
-        confirmFirstPiece();
+        cancelPiece();
         try {
             Thread.sleep(2000);
         } catch (Exception e){
@@ -113,4 +134,18 @@ public class RobotiumTests extends
         assertNotSame("X Position of piece must be different", result[0][1], result[1][1]);
 
     }
+
+    /*
+    //Check to see if player can still play the piece. If not, then the test is successful.
+    public void testConfirmFirstPiece(){
+        dragFirstPieceToCorner();
+        confirmPiece();
+
+    }
+    //Check to see if player can still play the piece. If not, then the test is successful.
+    public void testCancelFirstPiece(){
+        dragFirstPieceToCorner();
+        confirmPiece();
+
+    }*/
 }
