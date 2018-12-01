2bd222b90cfaa35a0d3f608aa5000214daf6ded4
Gabetn <gabriel.negash@mail.mcgill.ca>
2018-12-01 00:16:47 -0500

    Added testScoreUpdate, correctly working, and commented code

--- mobile/src/androidTest/java/org/scoutant/blokish/test/RobotiumTests.java
+++ mobile/src/androidTest/java/org/scoutant/blokish/test/RobotiumTests.java
@@ -4,12 +4,15 @@ import android.test.ActivityInstrumentationTestCase2;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.robotium.solo.Solo;
 
 import org.scoutant.blokish.*;
 import org.scoutant.blokish.R;
 import org.scoutant.blokish.model.Square;
+
+import java.util.Arrays;
 
 import cucumber.api.java.en.Given;
 
@@ -19,103 +22,30 @@ public class RobotiumTests extends
 
     private Solo solo;
     private UI ui;
-    private final String FIRST_PIECE_TYPE = "I5";
-    private final int COLOR = 0;
+    private static final String FIRST_PIECE_TYPE = "I5"; //Vertical Line (Top Left 1st piece available)
+    private static final int COLOR = 0; //Red
 
-
-    public RobotiumTests() {
+    public RobotiumTests(){
         super(UI.class);
     }
 
+    @Override
     public void setUp() throws Exception {
         super.setUp();
         solo = new Solo(getInstrumentation(), getActivity());
-
-
+        ui = (UI) getActivity();
     }
 
 
     @Override
     public void tearDown() throws Exception {
         solo.finishOpenedActivities();
+        super.tearDown();
     }
 
-    private int[][] dragFirstPieceToCorner(){
-
-        solo.waitForActivity("UI", 2000);
-        ui = (UI) getActivity();
-
-        PieceUI piece = ui.game.findPiece(0, FIRST_PIECE_TYPE);
-
-        int[][] result = new int[2][2];
-
-        int[] start=new int[2];
-        int[] end=new int[2];
-
-        piece.getLocationOnScreen(start);
-
-        float startX=(float)start[0]+ piece.getWidth()/2;
-        float startY =(float)start[1]+ piece.getHeight()-1;
-
-
-        float endX = 0;
-        float endY = 0;
-
-        for(Square s: ui.game.game.boards.get(0).seeds()){
-            endX = s.i*ui.game.size + ui.game.size/4;
-            endY = s.j*ui.game.size + 6*ui.game.size;
-
-            break;
-        }
-
-        System.out.println("Starting Location: "+startX +" " + startY);
-        solo.drag(startX, endX, startY, endY, 200);
-        solo.assertCurrentActivity("UI Supposed to Launch", UI.class);
-
-        piece.getLocationOnScreen(end);
-        result[0] = start;
-        result[1] = end;
-        return result;
-    }
-
-    private void confirmPiece(){
-        int[] position = {10, 10};
-        int width = 0;
-        for (int i=0; i<ui.game.getChildCount(); i++) {
-            View v = ui.game.getChildAt(i);
-            if (v instanceof ButtonsView) {
-
-                ButtonsView myButtonView = (ButtonsView) v;
-                myButtonView.ok.getLocationOnScreen(position);
-                width = myButtonView.ok.getWidth();
-                System.out.println("Found Button= "+(position[0] + width/2) +" " + (position[1] + width/2));
-                solo.clickOnScreen(position[0] + width/2, position[1] + width/2);
-                break;
-            }
-
-
-        }
-    }
-    private void cancelPiece(){
-        int[] position = {10, 10};
-        int width = 0;
-        for (int i=0; i<ui.game.getChildCount(); i++) {
-            View v = ui.game.getChildAt(i);
-            if (v instanceof ButtonsView) {
-
-                ButtonsView myButtonView = (ButtonsView) v;
-                myButtonView.ok.getLocationOnScreen(position);
-                width = myButtonView.ok.getWidth();
-                position[0] =  ui.game.size*20 - (position[0] + width/2);
-                System.out.println("Found Button= "+(position[0] + width/2) +" " + (position[1] + width/2));
-                solo.clickOnScreen(position[0], position[1] + width/2);
-                break;
-            }
-
-
-        }
-    }
-
+    /** TEST
+     *  Tests Scenario #X ,Feature: Drag Block Placement
+     */
     public void testDragFeature(){
 
         int result[][] = dragFirstPieceToCorner();
@@ -135,7 +65,32 @@ public class RobotiumTests extends
 
     }
 
-    /*
+    /** TEST
+     *  Tests Scenario #12, Feature:  Update Game Score
+     *  "Successfully add block to the board and update my score"
+     */
+    public void testScoreUpdate(){
+        TextView redTab = ui.game.tabs[0]; //holds view containing red player's score
+        
+        int preUpdateScore = 0;
+        if(!redTab.getText().toString().isEmpty()){ //score stored as string, initially empty string
+            preUpdateScore = Integer.parseInt(redTab.getText().toString());
+        }
+
+        int[][] moveResult = dragFirstPieceToCorner();
+        System.out.println("Move locations: \t"+Arrays.deepToString(moveResult)); //ToDO validate result of moving inital piece
+        confirmPiece();
+        solo.sleep(2000); // 2 seconds to ensure ui updated
+
+        int postUpdateScore = 0;
+        if(!redTab.getText().toString().isEmpty()){
+            postUpdateScore = Integer.parseInt(redTab.getText().toString());
+        } 
+
+        assertEquals(5, postUpdateScore - preUpdateScore); //I.e. score increased by 5
+    }
+
+    /* TODO: Implement
     //Check to see if player can still play the piece. If not, then the test is successful.
     public void testConfirmFirstPiece(){
         dragFirstPieceToCorner();
@@ -145,7 +100,102 @@ public class RobotiumTests extends
     //Check to see if player can still play the piece. If not, then the test is successful.
     public void testCancelFirstPiece(){
         dragFirstPieceToCorner();
-        confirmPiece();
+        cancelPiece();
 
     }*/
+
+    //HELPERS: -----------------------------------------------------------------------------------------
+
+    /** Helper
+     *  Drags the first piece in a new game to the upper left seed
+     *  Assumes Red Player, Assumes new game, Assumes screen hasn't been altered beforehand
+     * @return  int[0] before = array of pixel locations before piece was moved, I.e. before[0] = old X, before[1] = old Y
+     *          int[1] after = array of pixel locations after piece was moved, I.e. after[1] = new X, after[1] = new Y
+     */
+    private int[][] dragFirstPieceToCorner(){
+
+        solo.waitForActivity("UI", 2000);
+
+        PieceUI piece = ui.game.findPiece(COLOR, FIRST_PIECE_TYPE);
+
+        int[][] result = new int[2][2];
+
+        int[] start=new int[2];
+        int[] end=new int[2];
+
+        /* Writes into provided array the pixel location associated to the pieceUI        
+         * E.g. start[0] = x coordinate of left side of piece
+         *      start[1] = y coordinate of top of piece
+         */
+        piece.getLocationOnScreen(start);
+
+        float startX=(float)start[0]+ piece.getWidth()/2;
+        float startY =(float)start[1]+ piece.getHeight()-1;
+
+
+        float endX = 0; //drag requires float
+        float endY = 0;
+
+        //Note: in a new game the seeds() will return 1 Seed, the initial move
+        for(Square s: ui.game.game.boards.get(0).seeds()){
+            endX = s.i*ui.game.size + ui.game.size/4;
+            endY = s.j*ui.game.size + 6*ui.game.size;
+
+            break;
+        }
+
+        //System.out.println("Starting Location: "+startX +" " + startY);
+        solo.drag(startX, endX, startY, endY, 20);
+        //solo.assertCurrentActivity("UI Supposed to Launch", UI.class);
+
+        piece.getLocationOnScreen(end);
+        result[0] = start;
+        result[1] = end;
+        return result;
+    }
+
+    /** Helper
+     *  Will attempt to click the location where the confirm placement button is
+     */
+    private void confirmPiece(){
+        int[] position = {10, 10};
+        int width = 0;
+        for (int i=0; i<ui.game.getChildCount(); i++) {
+            View v = ui.game.getChildAt(i);
+            if (v instanceof ButtonsView) {
+
+                ButtonsView myButtonView = (ButtonsView) v;
+                myButtonView.ok.getLocationOnScreen(position);
+                width = myButtonView.ok.getWidth();
+                System.out.println("Found Button= "+(position[0] + width/2) +" " + (position[1] + width/2));
+                solo.clickOnScreen(position[0] + width/2, position[1] + width/2);
+                break;
+            }
+
+
+        }
+    }
+
+    /** Helper
+     *  Will attempt to click the location where the cancel placement button is
+     */
+    private void cancelPiece(){
+        int[] position = {10, 10};
+        int width = 0;
+        for (int i=0; i<ui.game.getChildCount(); i++) {
+            View v = ui.game.getChildAt(i);
+            if (v instanceof ButtonsView) {
+
+                ButtonsView myButtonView = (ButtonsView) v;
+                myButtonView.ok.getLocationOnScreen(position);
+                width = myButtonView.ok.getWidth();
+                position[0] =  ui.game.size*20 - (position[0] + width/2);
+                System.out.println("Found Button= "+(position[0] + width/2) +" " + (position[1] + width/2));
+                solo.clickOnScreen(position[0], position[1] + width/2);
+                break;
+            }
+
+
+        }
+    }
 }
