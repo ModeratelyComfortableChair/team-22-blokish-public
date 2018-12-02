546e23289c8d83e3f2f58707a9c65d2f7cb9cffd
Gabetn <gabriel.negash@mail.mcgill.ca>
2018-12-01 22:44:50 -0500

    travis emulator download script added

---
+++ emulatorSetup.sh
@@ -0,0 +1,4 @@
+wget http://dl.google.com/android/android-sdk_r21.1-linux.tgz
+tar xzf android-sdk_r21.1-linux.tgz
+export ANDROID_HOME=$PWD/android-sdk-linux
+export PATH=${PATH}:${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools
